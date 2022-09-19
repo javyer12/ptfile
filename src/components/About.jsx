d861664ba8223868f49cfb4261b42eb2593348bd
javyer <70788090+javyer12@users.noreply.github.com>
2022-09-18 22:45:21 -0600

    colors changed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -42,31 +42,34 @@ const About = () => {
           <div className="about__cards">
             <article className="about__card">
               <IoIosCodeWorking className="about__icon" />
-              <h5>Experience</h5>
+              <h6>Experience</h6>
               <small>1+ Years Working</small>
             </article>
             <article className="about__card hidded_class">
               <FiUsers className="about__icon" />
-              <h5>Clients</h5>
+              <h6>Clients</h6>
               <small>200+ Worldwide</small>
             </article>
             <article className="about__card hidded_class">
               <AiOutlineFundProjectionScreen className="about__icon" />
-              <h5>Personal Brand</h5>
+              <h6>Personal Brand</h6>
               <small>Physoft</small>
             </article>
             <article className="about__card">
               <IoMdDoneAll className="about__icon" />
-              <h5>Projects</h5>
+              <h6>Projects</h6>
               <small>50+ Completed</small>
             </article>
           </div>
-          <p>
-            I am a person who loves evolution, human and <span className="highlight__word">technological development</span>, lover of new technologies that help us build a <span className="highlight__word">digital world </span>in the near future, I am a frontend dev-junior with <span className="highlight__word">JavaScript, React js</span> others, passionate about it sports and the 7 arts.
-          </p>
-          <a className="btn btn-primary" href="#contact">
-            Let's Talk
-          </a>
+          <div className="about__desc">
+
+            <p>
+              I am a person who loves evolution, human and <span className="highlight__word">technological development</span>, lover of new technologies that help us build a <span className="highlight__word">digital world </span>in the near future, I am a frontend dev-junior with <span className="highlight__word">JavaScript, React js</span> others, passionate about it sports and the 7 arts.
+            </p>
+            <a className="btn btn-primary" href="#contact">
+              Let's Talk
+            </a>
+          </div>
         </div>
       </div>
     </section>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
 import "../style/Header.css";
 import CTA from "./CTA";
@@ -11,32 +11,43 @@ function Header() {
   return (
     <header id="home">
       <div className="container header__contianer">
-        <h2>Hello, I'm</h2>
-        <h1>Francisco Murillo</h1>
-        <h4 className="text-light"> Front-End Developer </h4>
-        <CTA />
-        <HeaderSocial />
+        <div className="header__info">
+          <h3>Hello, I'm</h3>
+          <h1>Francisco Murillo</h1>
+          <h4 className=""> Front-End Developer </h4>
+        </div>
+        <div className="cta__header">
+          <CTA />
+        </div>
+        <div className="social__header">
+          <HeaderSocial />
+        </div>
+
         <div className="me">
           <img src={me} alt="me" />
         </div>
-        <a
-          href={scroll}
-          onClick={() => {
-            if (scroll === "#contact") {
-              setScroll("#home");
-            } else {
-              setScroll("#contact");
-            }
-            console.log(scroll);
-          }}
-          className="scroll__down"
-        >
-          {scroll === "#contact" ? (
-            <h4 style={{ color: "#4db5ff", fontSize: "15px" }}>Scroll Up</h4>
-          ) : (
-            <h4 style={{ color: "#b9b8b5", fontSize: "15px" }}>Scroll Down</h4>
-          )}
-        </a>
+
+        <div className="scroll">
+          <a
+            href={scroll}
+            onClick={() => {
+              if (scroll === "#contact") {
+                setScroll("#home");
+              } else {
+                setScroll("#contact");
+              }
+            }}
+            className="scroll__down"
+          >
+            {scroll === "#contact" ? (
+              <h4 style={{ color: "#4db5ff", fontSize: "15px" }}>Scroll Up</h4>
+            ) : (
+              <h4 style={{ color: "#b9b8b5", fontSize: "15px" }}>
+                Scroll Down
+              </h4>
+            )}
+          </a>
+        </div>
       </div>
     </header>
   );
