45eeb385149d34a327c2c18f3fc61ec4af5ee142
javyer <70788090+javyer12@users.noreply.github.com>
2023-01-29 22:00:21 -0600

    change

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-// import { Box, Grid, Button, Tooltip } from '@mui/material/';
-import Tooltips from "./blocks/Tooltip";
-// import Box from '@mui/material/Box';
-// import Grid from '@mui/material/Grid';
-// import Button from '@mui/material/Button';
-// import Tooltip from '@mui/material/Tooltip';
 import logo from "../material/logo-blue.png";
 import { BsCodeSlash } from "react-icons/bs";
 import { SiAltiumdesigner } from "react-icons/si";
